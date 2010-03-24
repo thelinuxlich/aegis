59f1a5276d511cbea36ba99e65c41de4bf978acc
Alisson <thelinuxlich@gmail.com>
2010-03-24 12:15:42 -0300

    Fix on permission_filter

--- lib/aegis/permission_filter.rb
+++ lib/aegis/permission_filter.rb
@@ -7,7 +7,7 @@ module Aegis
         end
 
         protected
-          def authorize_action(current_user)
+          def authorize_action(current_user,option = {})
             return if options[:except].include? controller_name
             permission_type = ""
             case action_name
